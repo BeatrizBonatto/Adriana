d4ebd2e73f90a4743012d079998275bc5091a9f9
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2023-08-17 21:54:48 -0300

    Atividades
    ex

---
+++ Linguagem e Lab II/ex1.java
@@ -0,0 +1,11 @@
+public class ex1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("a= "+a+" ,  b= "+b);
+
+        int c=a;
+        int d=b;
+        System.out.println("a= "+d+" ,  b= "+c);
+    }
+}
