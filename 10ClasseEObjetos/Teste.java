49f4dede137f7cfe41b1ba9a2a277e0d55d68c80
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2023-10-18 12:21:35 -0300

    Update Teste.java

--- 10ClasseEObjetos/Teste.java
+++ 10ClasseEObjetos/Teste.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Teste {
     public static void main(String[] args) {
+        System.out.println("Objetos");
         System.out.println();
-        System.out.println("Gatos:");
-        System.out.println();
-
+        
+        System.out.println("Alaude");
         Alaude alaude = new Alaude(6, "Madeira", true);
         System.out.println("O número de cordas do alaude é: " + alaude.getCordas());
         System.out.println("O material do alaude é: " + alaude.getMaterial());
@@ -15,7 +15,8 @@ public class Teste {
         alaude.afinar("Sol");
         alaude.tocar(true);
         alaude.historia("1");
-        
+
+        System.out.println("Celular");
         Celular celular = new Celular("iPhone 13", "Apple", 5000);
         System.out.println("O modelo do celular é: " + celular.getModelo());
         System.out.println("A marca do celular é: " + celular.getMarca());
@@ -24,12 +25,20 @@ public class Teste {
         celular.discar("11999999999");
         celular.mandarMensagem("11999999999", "Olá, tudo bem?");
         celular.ligar(false);
-        
+
+        System.out.println("Borracha");
         Borracha borracha = new Borracha("Vermelha", "Lápis de Cor", false);
         System.out.println(borracha.apagar());
         System.out.println(borracha.borrar());
         System.out.println(borracha.esfregar());
-        
+
+        System.out.println("Estojo");
+        Estojo estojo = new Estojo("Vermelho", 5);
+        estojo.abrir(true);
+        estojo.guardar(5, "Lápis");
+        estojo.abrir(false);
+
+        System.out.println("Gato");
         Gato gato = new Gato("preto", "curto", 3);
         gato.correr(10);
         System.out.println(gato.irritado());
