1177b77f76e6d56fcf68e1d7fcd835c0e2df6bc6
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2023-10-18 11:15:25 -0300

    Update Teste.java

--- 10ClasseEObjetos/Teste.java
+++ 10ClasseEObjetos/Teste.java
@@ -8,6 +8,20 @@ public class Teste {
         System.out.println("Gatos:");
         System.out.println();
 
+        Alaude alaude = new Alaude(6, "Madeira", true);
+        System.out.println("O número de cordas do alaude é: " + alaude.getCordas());
+        System.out.println("O material do alaude é: " + alaude.getMaterial());
+        System.out.println("O alaude é analógico: " + alaude.analogico());
+        alaude.afinar("Sol");
+        alaude.tocar(true);
+        alaude.historia("1");
+        
+        
+        Borracha borracha = new Borracha("Vermelha", "Lápis de Cor", false);
+        System.out.println(borracha.apagar());
+        System.out.println(borracha.borrar());
+        System.out.println(borracha.esfregar());
+        
         Gato gato1 = new Gato("preto", "curto", 3);
         gato.correr(10);
         System.out.println(gato.irritado());
